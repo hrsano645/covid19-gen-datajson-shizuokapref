2fd764b372eaaea0d347de3be653f97430100465
Hiroshi Sano <hrs.sano645@gmail.com>
2020-04-27 14:04:07 +0000

    python3ベースでエンコード指定でcsvファイルを開くようにした

--- bat.sh
+++ bat.sh
@@ -1,9 +1,13 @@
-curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44809/220001_shizuoka_covid19_call_center.csv > 220001_shizuoka_covid19_call_center.csv
-curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44807/220001_shizuoka_covid19_patients.csv > 220001_shizuoka_covid19_patients.csv
-curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44810/220001_shizuoka_covid19_test_people.csv > 220001_shizuoka_covid19_test_people.csv
+# curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44809/220001_shizuoka_covid19_call_center.csv > 220001_shizuoka_covid19_call_center.csv
+# curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44807/220001_shizuoka_covid19_patients.csv > 220001_shizuoka_covid19_patients.csv
+# curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44810/220001_shizuoka_covid19_test_people.csv > 220001_shizuoka_covid19_test_people.csv
 
-nkf -w8 220001_shizuoka_covid19_patients.csv > patients.csv
-nkf -w8 220001_shizuoka_covid19_call_center.csv > call_center.csv
-nkf -w8 220001_shizuoka_covid19_test_people.csv > test_people.csv
+# nkf -w8 220001_shizuoka_covid19_patients.csv > patients.csv
+# nkf -w8 220001_shizuoka_covid19_call_center.csv > call_center.csv
+# nkf -w8 220001_shizuoka_covid19_test_people.csv > test_people.csv
+
+curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44809/220001_shizuoka_covid19_call_center.csv > call_center.csv
+curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44807/220001_shizuoka_covid19_patients.csv > patients.csv
+curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44810/220001_shizuoka_covid19_test_people.csv > test_people.csv
 
 python patients.py patients.csv test_people.csv call_center.csv > data.json
