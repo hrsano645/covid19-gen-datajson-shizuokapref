31957c8487082666fe6f6d60f1388d8850f72126
Hiroshi Sano <hrs.sano645@gmail.com>
2020-06-04 02:17:34 +0000

    検査実施件数向けファイルの引数を仮設置

--- bat.sh
+++ bat.sh
@@ -1,5 +1,6 @@
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44809/220001_shizuoka_covid19_call_center.csv > call_center.csv
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44807/220001_shizuoka_covid19_patients.csv > patients.csv
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/44810/220001_shizuoka_covid19_test_people.csv > test_people.csv
+# TODO:2020/06/04 inspections_summaryが提供されたらcrulでDLする
 
-python patients.py patients.csv test_people.csv call_center.csv > data.json
+python patients.py patients.csv test_people.csv call_center.csv inspections_summary.csv > data.json
