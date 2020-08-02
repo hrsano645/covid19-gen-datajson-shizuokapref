8100cf2950e0ef544e74334f573497461296e50a
Hiroshi Sano <hrs.sano645@gmail.com>
2020-08-02 13:08:44 +0000

    fix fuction name

--- tests/test_varidate_opendata_dataformat.py
+++ tests/test_varidate_opendata_dataformat.py
@@ -1,9 +1,9 @@
-from patients import varidate_opendata_dateformat
+from patients import validate_opendata_dateformat
 
 
 def test_correct():
-    assert ("2020", "1", "1") == varidate_opendata_dateformat("2020/1/1")
+    assert ("2020", "1", "1") == validate_opendata_dateformat("2020/1/1")
 
 
 def test_notcorrect():
-    assert None == varidate_opendata_dateformat("/2020/1/1")
+    assert None == validate_opendata_dateformat("/2020/1/1")
