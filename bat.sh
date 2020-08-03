cbff146394c4c19f93bc5b8d5c310938bfc7ff16
Hiroshi Sano <hrs.sano645@gmail.com>
2020-08-03 00:24:04 +0000

    delete command redirect

--- bat.sh
+++ bat.sh
@@ -2,4 +2,4 @@ curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/46009/220001_shizuo
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/46279/220001_shizuoka_covid19_patients.csv > patients.csv
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/45876/220001_shizuoka_covid19_test_number.csv > test_number.csv
 
-python patients.py patients.csv call_center.csv test_number.csv > data.json
+python patients.py patients.csv call_center.csv test_number.csv
