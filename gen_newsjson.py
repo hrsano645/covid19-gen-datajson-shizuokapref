05f6c2ffa205f1c5015e215927b295fef68096ea
Hiroshi Sano <hrs.sano645@gmail.com>
2020-09-26 06:45:10 +0000

    ニュース記事の情報部分:新しい探索方法の実装

--- gen_newsjson.py
+++ gen_newsjson.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import json
 import sys
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,6 +16,18 @@ NEW_ITEM_PTAG_SELECTOR = (
 FILE_NAME = "news.json"
 
 
+def search_news_list(bs: BeautifulSoup) -> list:
+    """
+    静岡の対策サイトの新着情報を探索して、新着情報のリスト一覧を生成する
+
+    特定のタグを知りたいつもりだったけど、text属性に正規表現を渡して、日付が書いてある部分を探しに行く
+
+    > ・9/23 川勝知事からのメッセージを掲載しました
+    """
+
+    return bs.find_all(text=re.compile("・\\d{1,2}/\\d{1,2} .*"))
+
+
 def main():
     shizuoka_covid19_req = requests.get(TARGET_URL)
     shizuoka_covid19_req.encoding = shizuoka_covid19_req.apparent_encoding
@@ -24,24 +37,24 @@ def main():
         # TODO:2020-07-12 サイトのアクセスが出来ないエラーとして、slackに通知ポストする
         sys.exit(1)
 
-    # サイトの構造を取る
+    # サイトをBSでパース
     shizuoka_covid19_bs = BeautifulSoup(
         shizuoka_covid19_req.text, features="html.parser"
     )
 
     # なるべくサイトの構造に依存しないでニュース記事を取りに行く
-    news_p_tag_list = shizuoka_covid19_bs.select(NEW_ITEM_PTAG_SELECTOR)
+    news_tag_list = search_news_list(shizuoka_covid19_bs)
 
     # サイトの構造が変わり、pタグが取得できない場合はエラーとして終了する
-    if not news_p_tag_list:
+    if not news_tag_list:
         # TODO:2020-07-12 サイトのアクセスが出来ないエラーとして、slackに通知ポストする
         print("エラーが発生しました:サイトの構造が変更されたようです")
         sys.exit(1)
 
     news_list = list()
-    for news_p_tag in news_p_tag_list:
+    for news_p_tag in news_tag_list:
 
-        news_a_tag = news_p_tag.find("a")
+        news_a_tag = news_p_tag.find_next()
         news_link = news_a_tag["href"]
 
         # urlの正規化:絶対アドレスではない場合の処理
@@ -50,13 +63,16 @@ def main():
 
         item = {
             # 日付文字列の先頭に"・"、最後に" "があるから省く
-            "date": news_p_tag.contents[0][1:],
+            "date": news_p_tag[1:-1],
             "url": news_link,
             "text": news_a_tag.text,
         }
         news_list.append(item)
 
-    # TODO:2020-07-12 サイトの構造が中途半端に変更があったとき用に値のバリデーションを入れたほうがいいかも。
+    # from pprint import pprint
+
+    # pprint(news_list)
+    # exit()
 
     # ここまで来たらjsonファイルに保存する
     with open(FILE_NAME, "w", encoding="utf-8") as newsjson:
