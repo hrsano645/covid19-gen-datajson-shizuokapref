3394e4c8cc99890f0ae879e3d255fb6b309097c7
Hiroshi Sano <hrs.sano645@gmail.com>
2020-11-27 03:02:34 +0000

    remove comment

--- tests/test_main_summary.py
+++ tests/test_main_summary.py
@@ -1,23 +1,5 @@
 from patients import gen_main_summary_data
 
-# {
-#     "陽性患者数": (
-#         case_count_list["0"]
-#         + case_count_list["2"]
-#         + case_count_list["3"]
-#         + case_count_list["4"]
-#         + case_count_list["5"]
-#     ),  # 陽性患者数 = 入院 + 療養 + 調整中 + 死亡 + 退院
-#     "入院中": case_count_list["0"],
-#     "軽症・中等症": case_count_list["0"]
-#     - case_count_list["1"],  # 軽症・中等症 = 入院中 - うち重症
-#     "重症": case_count_list["1"],
-#     "宿泊療養": case_count_list["2"],
-#     "入院・療養等調整中": case_count_list["3"],
-#     "死亡": case_count_list["4"],
-#     "退院": case_count_list["5"],
-# }
-
 
 def test_gen_main_summary_data():
 
