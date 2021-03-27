cfb122ac080082f717059c9b9d9a5f82a33d6687
Hiroshi Sano <hrs.sano645@gmail.com>
2021-03-27 21:18:46 +0900

    Squashed commit of the following:
    commit 210e858cf6e18efe97d4776facba621d8363de87
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Thu Mar 25 21:38:04 2021 +0900
    
        remove blank line
    
    commit d38a142e50c6da6942557a179e7b3cec041bca39
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Thu Mar 25 21:13:37 2021 +0900
    
        add script progress
    
    commit 7d06a144881041163a1b2849987ad93da63b0556
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Thu Mar 25 21:07:48 2021 +0900
    
        fix default localname
    
    commit 047945c0826025d0d0e253a845b64e4025272cc2
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Thu Mar 25 21:06:45 2021 +0900
    
        add python venv. generate, activate
    
    commit 8fe30684a1194d57ad5686f8670b877ee396a39e
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Mon Mar 22 21:51:59 2021 +0900
    
        update dependency packages
    
    commit 9b1757a3353000755010a779143b9f983cedd1a9
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Sun Mar 21 22:05:17 2021 +0900
    
        add yaer to news date
    
    commit e80a71da61bb3b39fe0d6dfb23bc571328c69140
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Fri Mar 19 21:31:41 2021 +0900
    
        add docstring
    
    commit 08f3df2f00548462d8e2b1c79cf678e68c97ac73
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Fri Mar 19 21:22:32 2021 +0900
    
        コメント整理
    
    commit 3fce0b90ca1c8560c9d052c80b95c960052f389f
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Fri Mar 19 21:00:51 2021 +0900
    
        コメント書いたり消したり
    
    commit 481ed8d0b25a678a99b102e85967304735ffc0f2
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Fri Mar 19 20:55:04 2021 +0900
    
        とりあえず動いた
    
    commit 78eceda248761b4f73b03e12490574a2a8f58038
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Fri Mar 19 12:12:21 2021 +0900
    
        wip: add gen_cls, genfuncs
    
    commit b69360f9f12cf8f455ad9f6ffee61e9a5bf7d0e0
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Wed Mar 17 16:30:13 2021 +0900
    
        add class bluk
    
    commit caeef9c3e029eb9e05837503896dbc86eb2a995b
    Author: Hiroshi Sano <hrs.sano645@gmail.com>
    Date:   Wed Mar 17 16:20:27 2021 +0900
    
        add comment

--- gen_newsjson.py
+++ gen_newsjson.py
@@ -2,73 +2,157 @@
 import json
 import sys
 import re
+from collections import namedtuple
+import io
+import csv
+import datetime
 
 import requests
 from bs4 import BeautifulSoup
 
-# 新着情報を入手するサイトのURL
-TARGET_URL = "https://www.pref.shizuoka.jp/kinkyu/covid-19.html"
-
-# 出力するファイル名
-FILE_NAME = "news.json"
+# 地域名と対応するNewsクラスをセットするコンテナ
+LocalGenCls = namedtuple("LocalGenCls", ["name", "cls"])
 
 
-def search_news_list(bs: BeautifulSoup) -> list:
+class News(object):
     """
-    静岡の対策サイトの新着情報を探索して、新着情報のリスト一覧を生成する
-
-    特定のタグを知りたいつもりだったけど、text属性に正規表現を渡して、日付が書いてある部分を探しに行く
-
-    > ・9/23 川勝知事からのメッセージを掲載しました
+    news.jsonを生成するクラス。必要なデータを取得する関数を渡してインスタンス化し、generate_jsonメソッドを実行することにより、news.jsonを生成します。
+    
+    news_read_funcは戻り値に"date", "url", "text" の三つのkeyが入る辞書オブジェクトのリストである必要があります。
+    # TODO:2021-03-19 news_read_funcの戻り値は現在は厳密に調べていません。実装予定です。
     """
 
-    return bs.find_all(text=re.compile("・\\d{1,2}/\\d{1,2}.*"))
+    def __init__(self, news_read_func):
+        self.news_read_func = news_read_func
+        self.news_item_list = []
+
+    def _get_newslist(self):
+        self.news_item_list = self.news_read_func()
+        if not self.news_item_list:
+            # error 的な物を返す
+            print("ニュースが1行も生成されませんでした")
+            sys.exit(1)
+
+    def generate_json(self):
+        self._get_newslist()
+        with open("news.json", "w", encoding="utf-8") as newsjson:
+            json.dump(
+                {"newsItems": self.news_item_list},
+                newsjson,
+                ensure_ascii=False,
+                indent=4,
+            )
 
 
-def main():
-    shizuoka_covid19_req = requests.get(TARGET_URL)
-    shizuoka_covid19_req.encoding = shizuoka_covid19_req.apparent_encoding
+def get_shizuoka_newslist() -> list:
+    """
+    静岡県の新型コロナ対策サイトの新着情報を、静岡県の新型コロナの情報サイトからスクレイピングしてnews.jsonに使うデータを作成します
+    なるべく失敗しにくくするために、正規表現を使って特定のタグを取り込めるようにしています。
+    
+    例:
+    <p>・3/19 <a href="/kinkyu/covid-19-tyuumokujouhou.html">新型コロナウイルス陽性者が13名確認されました</a></p>
 
-    if not shizuoka_covid19_req.status_code == 200:
-        print("エラーが発生しました:サイトへのアクセスが出来ませんでした。")
-        # TODO:2020-07-12 サイトのアクセスが出来ないエラーとして、slackに通知ポストする
+    結果は"date", "url", "text" の三つのkeyが入る辞書オブジェクトのリストが作成されます。
+    取得に失敗した場合は例外として終了します。
+    """
+    # 新着情報を入手するサイトのURL
+    TARGET_URL = "https://www.pref.shizuoka.jp/kinkyu/covid-19.html"
+
+    req = requests.get(TARGET_URL)
+    req.encoding = req.apparent_encoding
+
+    if not req.status_code == 200:
+        # TODO:2021-03-19 ここのprintは例外として処理する。NotConnectError的な例外名
+        print("サイトへのアクセスが出来ませんでした: status_code:{}".format(req.status_code))
         sys.exit(1)
 
     # サイトをBSでパース
-    shizuoka_covid19_bs = BeautifulSoup(
-        shizuoka_covid19_req.text, features="html.parser"
-    )
-
-    # なるべくサイトの構造に依存しないでニュース記事を取りに行く
-    news_tag_list = search_news_list(shizuoka_covid19_bs)
+    bs = BeautifulSoup(req.text, features="html.parser")
 
     # サイトの構造が変わり、pタグが取得できない場合はエラーとして終了する
+    news_tag_list = bs.find_all(text=re.compile("・\\d{1,2}/\\d{1,2}.*"))
     if not news_tag_list:
-        # TODO:2020-07-12 サイトのアクセスが出来ないエラーとして、slackに通知ポストする
-        print("エラーが発生しました:サイトの構造が変更されたようです")
+        # TODO:2021-03-19 ここのprintは例外として処理する。CantParseSiteError的な例外名
+        print("サイトの構造が変更されたようです")
         sys.exit(1)
 
     news_list = list()
     for news_p_tag in news_tag_list:
-
         news_a_tag = news_p_tag.find_next()
-        news_link = news_a_tag["href"]
 
         # urlの正規化:絶対アドレスではない場合の処理
+        news_link = news_a_tag["href"]
         if "http" not in news_link:
             news_link = "https://www.pref.shizuoka.jp" + news_a_tag["href"]
 
+        # 日付に西暦を加える
+        # 日付文字列の先頭に"・"、最後に" "があるから省く
+        news_date = datetime.datetime.strptime(news_p_tag.strip()[1:], "%m/%d")
+        # 無条件に今年の西暦に変更
+        now_dt = datetime.datetime.now()
+        news_date = news_date.replace(year=now_dt.year)
+
+        # 今の月より大きい月がある場合は無条件で去年にする
+        if now_dt.month < int(news_date.month):
+            news_date = news_date.replace(year=now_dt.year - 1)
         item = {
-            # 日付文字列の先頭に"・"、最後に" "があるから省く
-            "date": news_p_tag.strip()[1:],
+            "date": news_date.strftime("%Y/%m/%d"),
             "url": news_link,
             "text": news_a_tag.text,
         }
         news_list.append(item)
+    return news_list
 
-    # ここまで来たらjsonファイルに保存する
-    with open(FILE_NAME, "w", encoding="utf-8") as newsjson:
-        json.dump({"newsItems": news_list}, newsjson, ensure_ascii=False, indent=4)
+
+def get_fujicity_newslist() -> list:
+    """
+
+    富士市の新型コロナ対策サイトに使う新着情報を富士市のオープンデータから取得してnews.jsonに使うデータを作成します
+    結果は"date", "url", "text" の三つのkeyが入る辞書オブジェクトのリストが作成されます。
+    取得に失敗した場合は例外として終了します。
+    """
+
+    TARGET_URL = "https://opendata.pref.shizuoka.jp/dataset/8484/resource/50885/%E5%AF%8C%E5%A3%AB%E5%B8%82%E6%96%B0%E5%9E%8B%E3%82%B3%E3%83%AD%E3%83%8A%E3%82%A6%E3%82%A4%E3%83%AB%E3%82%B9%E9%96%A2%E9%80%A3%E3%83%8B%E3%83%A5%E3%83%BC%E3%82%B9.csv"
+    req = requests.get(TARGET_URL)
+    # requestのencodingがDLするファイルと合わないので、
+    req.encoding = req.apparent_encoding
+
+    if not req.status_code == 200:
+        # TODO:2021-03-19 ここのprintは例外として処理する。NotConnectError的な例外名
+        print("サイトへのアクセスが出来ませんでした: status_code:{}".format(req.status_code))
+        sys.exit(1)
+
+    news_list = []
+    # 富士市のオープンデータには列ヘッダがないので、あらかじめ指定する
+    with io.StringIO(req.text) as csvfile:
+        news_list = list(csv.DictReader(csvfile, ("date", "url", "text")))
+
+    return news_list
+
+
+def main():
+    # 地域名と実行する生成クラスを用意
+    local_func_map = [
+        LocalGenCls("静岡県", News(get_shizuoka_newslist)),
+        LocalGenCls("富士市", News(get_fujicity_newslist)),
+    ]
+
+    # ---[地域別フィルタ]---
+    local_name = None
+    if len(sys.argv) == 2:
+        local_name = sys.argv[1]
+    # 地域名がない場合は静岡県
+    if not local_name:
+        local_name = "静岡県"
+
+    # 地域名を元にフィルターして実行
+    for local_func in local_func_map:
+        if local_name == local_func.name:
+            print("{}のnews.jsonを生成します".format(local_name))
+            local_func.cls.generate_json()
+            break
+
+    exit()
 
 
 if __name__ == "__main__":
