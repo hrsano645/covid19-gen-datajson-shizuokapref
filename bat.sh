48f45e446fe4bb2b3b4b8a372d6a62d000cfa5a2
Hiroshi Sano <hrs.sano645@gmail.com>
2021-04-21 22:54:49 +0900

    fix comment

--- bat.sh
+++ bat.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
+# [設定]
 # 地域名を指定するときにはLOCALNAME変数に＊＊市と入力してください
 LOCALNAME=""
 
-# run scripts
+# [スクリプトスタート]
 echo "===[地域名: ${LOCALNAME}]==="
 
-# check python venv and activate
+# システムにパッケージを入れないためにvenv環境を用意する。既にある場合は有効化
 cwd=$(cd $(dirname $0); pwd)
 cd ${cwd}
 
 if [ -d ${cwd}/.venv/ ]; then
-    # if not venv, generate venv
-    echo "===[activate python venv]==="
+    # venvがある場合は有効化
+    echo "===[Python venv 有効化]==="
     source ${cwd}/.venv/bin/activate
 
 else
-    echo "===[generate python venv]==="
+    echo "===[Python venv 作成]==="
     python3 -m venv .venv
     source ${cwd}/.venv/bin/activate
     pip install -r requirements.txt
 fi
 
-# 実行環境確認用
-echo "pythonpath: `which python`"
+# Python実行環境の表示
+echo "Using Pythonpath: `which python`"
 echo "pythonversion: `python --version`"
 
-# generate data.json
-echo "===[generate data.json]==="
+echo "===[data.json 生成]==="
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/46009/220001_shizuoka_covid19_call_center.csv > call_center.csv
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/46279/220001_shizuoka_covid19_patients.csv > patients.csv
 curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/45876/220001_shizuoka_covid19_test_number.csv > test_number.csv
@@ -35,6 +35,5 @@ curl https://opendata.pref.shizuoka.jp/dataset/8167/resource/48851/220001_new_sh
 
 python patients.py patients.csv call_center.csv test_number.csv details_of_confirmed_cases.csv ${LOCALNAME}
 
-#generate news.json
-echo "===[generate news.json]==="
+echo "===[news.json生成]==="
 python gen_newsjson.py ${LOCALNAME}
