53827b6fc9fe5d8c78f9f8961a64ff7beb035617
Hiroshi Sano <hrs.sano645@gmail.com>
2021-04-26 21:14:30 +0900

    fix test, update testdata

--- tests/test_perse.py
+++ tests/test_perse.py
@@ -15,13 +15,36 @@ def test_column_count_parse_call_center():
 def test_column_count_parse_patients():
 
     load_matrix = parse_patients("./tests/test_opendata/testdata_patients.csv")
-    # ファイルの列数
-    assert 17 == len(load_matrix[0])
+
+    column_header = [
+        "No",
+        "例目",
+        "全国地方公共団体コード",
+        "都道府県名",
+        "市区町村名",
+        "公表_年月日",
+        "発症_年月日",
+        "患者_居住地",
+        "患者_年代",
+        "患者_性別",
+        "患者_職業",
+        "患者_状態",
+        "患者_症状",
+        "患者_渡航歴の有無フラグ",
+        "患者_退院済フラグ",
+        "備考",
+    ]
+
+    for row in load_matrix:
+        # 全ての行で列が指定のヘッダ名あるかを調べる
+        assert column_header == list(row.keys())
 
 
 def test_column_count_parse_inspections_summary():
 
-    load_matrix = parse_inspections_summary("./tests/test_opendata/testdata_test_number.csv")
+    load_matrix = parse_inspections_summary(
+        "./tests/test_opendata/testdata_test_number.csv"
+    )
     # ファイルの列数
     assert 6 == len(load_matrix[0])
 
@@ -42,4 +65,3 @@ def test_parse_details_of_confirmed_cases():
     assert main_summary_counts["入院等調整中"] == 17
     assert main_summary_counts["死亡"] == 93
     assert main_summary_counts["退院"] == 4761
-
